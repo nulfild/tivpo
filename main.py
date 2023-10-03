fa43e90ebcd271f6b5b4b829cfa81ad39e8f50a3
Nulfild <kunin.german@icloud.com>
2023-10-03 21:36:21 +0300

    Очередной рефакторинг
    Поменял файлы местами

--- main.py
+++ main.py
@@ -24,27 +24,33 @@ class Game:
             words = ["вратарь", "черепаха", "бергамот", "баклажан", "колибри", "хибара"]
         return words
 
-    def kick_player(self, player_index):
+    def kick_player(self):
         print("Покидает игру " + self.players[self.currentPlayerIndex])
         self.players.pop(self.currentPlayerIndex)
         if self.currentPlayerIndex == len(self.players):
             self.currentPlayerIndex = 0
+
     def get_random_word(self):
         pass
+
     def check_input(self, inputText):
         return bool(re.search(r'^[а-яА-Я]+$', inputText))
-       
+
     def next_player(self):
         if self.currentPlayerIndex == len(self.players) - 1:
             self.currentPlayerIndex = 0
         else:
             self.currentPlayerIndex += 1
+
     def ask_for_input(self):
         pass
+
     def check_answer(self, guess):
         pass
+
     def make_turn(self):
         pass
+
     def start(self):
         pass
 
