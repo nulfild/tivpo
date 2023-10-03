cc7ed155bb96d42d16f757d72f1286224d04d17b
Nikita-neuron <077anderwater770@gmail.com>
2023-10-03 19:53:27 +0300

    BDD (Behavior Driven Development)

---
+++ TestNextPlayer.py
@@ -0,0 +1,25 @@
+import unittest
+from main import Game
+# from v1 import Game
+
+class TestNextPlayer(unittest.TestCase):
+    def test_next_player_inex_med(self):
+        game = Game()
+        game.players = ["Player 1", "Player 2", "Player 3"]
+        game.currentPlayerIndex = 1
+
+        game.next_player()
+
+        self.assertEqual(game.currentPlayerIndex, 2)
+
+    def test_next_player_inex_end(self):
+        game = Game()
+        game.players = ["Player 1", "Player 2", "Player 3"]
+        game.currentPlayerIndex = 2
+
+        game.next_player()
+
+        self.assertEqual(game.currentPlayerIndex, 0)
+
+if __name__ == "__main__":
+    unittest.main()
