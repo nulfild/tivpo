cf208b91f5a701d664a983bbf05b91cfe90a2079
Nulfild <kunin.german@icloud.com>
2023-10-04 10:07:36 +0300

    Изменил тесты для checkAnswer

--- checkAnswerTest.py
+++ checkAnswerTest.py
@@ -5,48 +5,49 @@ class TestCheckAnswerFunction(unittest.TestCase):
 
     def test_correct_single_letter_guess(self):
         game = Game()
-        game.currentWord = "apple"
-        game.currentWordForAnswer = "*****"
-        game.check_answer("a")
-        self.assertEqual(game.currentWordForAnswer, "a****")
+        game.currentWord = "яблоко"
+        game.currentWordForAnswer = "я****"
+        game.check_answer("я")
+        self.assertEqual(game.currentWordForAnswer, "я****")
 
     def test_incorrect_single_letter_guess(self):
         game = Game()
-        game.currentWord = "apple"
+        game.currentWord = "яблоко"
         game.currentWordForAnswer = "*****"
-        game.check_answer("x")
-        self.assertNotEqual(game.currentWordForAnswer, "x****")
+        game.check_answer("н")
+        self.assertNotEqual(game.currentWordForAnswer, "н****")
         
     def test_correct_full_word_guess(self):
         game = Game()
-        game.currentWord = "banana"
-        game.currentWordForAnswer = "******"
-        game.check_answer("banana")
-        self.assertEqual(game.currentWordForAnswer, "banana")
+        game.currentWord = "банан"
+        game.currentWordForAnswer = "*****"
+        game.check_answer("банан")
+        self.assertEqual(game.currentWordForAnswer, "банан")
 
     def test_incorrect_full_word_guess(self):
         game = Game()
         game.players = ["Player 1", "Player 2", "Player 3", "Player 4"]
-        game.currentWord = "cherry"
-        game.currentWordForAnswer = "******"
-        game.check_answer("grape")
-        self.assertNotEqual(game.currentWordForAnswer, "grape")
+        game.currentWord = "черри"
+        game.currentWordForAnswer = "*****"
+        game.check_answer("апельсин")
+        self.assertNotEqual(game.currentWordForAnswer, "черри")
+        self.assertEqual(len(game.players), 3)
 
     def test_letter_already_guessed(self):
         game = Game()
-        game.currentWord = "orange"
-        game.currentWordForAnswer = "******"
-        game.used_letters.add("o")
-        game.check_answer("o")
-        self.assertEqual(game.currentWordForAnswer, "o*****")
+        game.currentWord = "охрана"
+        game.currentWordForAnswer = "о*****"
+        game.used_letters.add("о")
+        game.check_answer("о")
+        self.assertEqual(game.currentWordForAnswer, "о*****")
         self.assertEqual(game.currentPlayerIndex, 0)
 
     def test_next_player_called_on_incorrect_guess(self):
         game = Game()
-        game.currentWord = "pear"
-        game.currentWordForAnswer = "____"
+        game.currentWord = "арбуз"
+        game.currentWordForAnswer = "*****"
         initial_player_index = game.currentPlayerIndex
-        game.check_answer("x")
+        game.check_answer("д")
         self.assertNotEqual(game.currentPlayerIndex, initial_player_index)
 
 if __name__ == '__main__':
